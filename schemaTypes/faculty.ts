787a0e287cf94986604f84338e1230d155c21e53
Lupin <luthfialfatih@gmail.com>
2026-06-29 13:54:51 +0700

    Name field deleted

--- schemaTypes/faculty.ts
+++ schemaTypes/faculty.ts
@@ -5,8 +5,7 @@ export default defineType({
   title: 'Faculty',
   type: 'document',
   fields: [
-    defineField({name: 'facultyname', title: 'Faculty Name', type: 'string'}),
-    defineField({name: 'name', title: 'Name', type: 'string', validation: (r) => r.required()}),
+    defineField({name: 'facultyname', title: 'Faculty Name', type: 'string', validation: (r) => r.required()}),
     defineField({name: 'role', title: 'Role', type: 'string'}),
     defineField({name: 'bio', title: 'Bio', type: 'text'}),
     defineField({name: 'avatar', title: 'Avatar', type: 'image', options: {hotspot: true}}),
